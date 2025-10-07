60f84fda8698eef3ee0ed6169fe55f7023ac4e12
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2025-10-07 18:42:50 +0200

    feat: add command-line argument parsing for server configuration

--- src/server/__main__.py
+++ src/server/__main__.py
@@ -1,4 +1,5 @@
 import asyncio
+import argparse
 
 from .web_server import WebServer
 from .assignment_manager import AssignmentManager
@@ -8,20 +9,32 @@ from .tcp_server import TcpServer
 from . import ASSIGNMENTS_FILE, PHYSICAL_PORTS, SOCKET_HOST, SOCKET_PORT, dispatcher, logger, WEB_HOST, WEB_PORT
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="USBIP Autobind Server")
+    parser.add_argument('--socket-host', type=str, default=SOCKET_HOST, help='Host for TCP server')
+    parser.add_argument('--socket-port', type=int, default=SOCKET_PORT, help='Port for TCP server')
+    parser.add_argument('--web-host', type=str, default=WEB_HOST, help='Host for web server')
+    parser.add_argument('--web-port', type=int, default=WEB_PORT, help='Port for web server')
+    parser.add_argument('--physical-ports', type=str, default=','.join(PHYSICAL_PORTS), help='Comma-separated list of physical ports')
+    parser.add_argument('--assignments-file', type=str, default=ASSIGNMENTS_FILE, help='Path to assignments file')
+    return parser.parse_args()
+
+
 async def main():
+    args = parse_args()
     main_loop = asyncio.get_event_loop()
 
-    assignment_manager = AssignmentManager(ASSIGNMENTS_FILE)
-    device_manager = DeviceManager(PHYSICAL_PORTS, main_loop, assignment_manager)
+    assignment_manager = AssignmentManager(args.assignments_file)
+    device_manager = DeviceManager(args.physical_ports.split(','), main_loop, assignment_manager)
     client_manager = ClientManager(device_manager, assignment_manager)
-    tcp_server = TcpServer(SOCKET_HOST, SOCKET_PORT, client_manager)
+    tcp_server = TcpServer(args.socket_host, args.socket_port, client_manager)
 
     device_manager.scan_existing_devices()
     device_manager.start_monitoring()
     socket_task = await tcp_server.start_server()
 
     dispatcher.subscribe("webui_start", lambda: logger.info("Application starting up..."))
-    web_server = WebServer(WEB_HOST, WEB_PORT, device_manager, assignment_manager, client_manager)
+    web_server = WebServer(args.web_host, args.web_port, device_manager, assignment_manager, client_manager)
     await web_server.run()
     socket_task.cancel()
     await asyncio.gather(socket_task, return_exceptions=True)
